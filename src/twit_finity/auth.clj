2d5ba15414814a30544d4e628a8133d6bbe54006
Angus Fletcher <ethdem@gmail.com>
2015-05-12 22:09:04 -0300

    Mostly idiomatic changes
    - read is basically the same as eval, so can evaluate untrusted code.
    The edn reader can load clojure data structures without evaluating the
    code, which is safer.
    
    -The io/read stuff is completely acceptable, most read functions have a
    read-string counterpart and slurp returns a string

--- src/twit_finity/auth.clj
+++ src/twit_finity/auth.clj
@@ -1,12 +1,11 @@
 (ns twit-finity.auth
-  (:require 
-    [clojure.java.io :as io]))
+  (:require
+    [clojure.edn :as edn]))
 
-(defn load-config 
+(defn load-config
   "Parses config file from text-file"
-  [filename] 
-  (with-open [r (io/reader filename)]
-    (read (java.io.PushbackReader. r))))
+  [filename]
+  (-> (slurp filename)
+      edn/read-string))
 
 (def credentials (load-config "config.clj"))
-
